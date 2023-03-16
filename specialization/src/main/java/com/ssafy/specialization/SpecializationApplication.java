4037d72c01c587efbeec18c17bae1fb8a28d82d3
왕현석 <sjj02055@gmail.com>
2023-03-16 10:16:13 +0900

    FEAT: Create skeleton project

---
+++ specialization/src/main/java/com/ssafy/specialization/SpecializationApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.specialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecializationApplication.class, args);
+	}
+
+}
---
+++ specialization/src/test/java/com/ssafy/specialization/SpecializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.specialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
