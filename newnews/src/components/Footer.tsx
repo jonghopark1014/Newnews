4c346ff3e8e6ee60439ddc89565ed1ddaa1c869f
ssg-js <likemath1997@gmail.com>
2023-03-20 20:58:23 +0900

    FEAT: FE_하단footer바_고정

--- newnews/src/components/Footer.tsx
+++ newnews/src/components/Footer.tsx
@@ -3,14 +3,27 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FiBookmark } from "react-icons/fi";
 import { HiUserCircle } from "react-icons/hi";
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import "../styles/FooterBar.scss"
+
+type navigation = 'home' | 'search' | 'bookmark' | 'mypage';
+type footerState = 'footer-navigator' | 'footer-navigator-now';
 
 export function Footer(){
+    const [page, setPage] = useState<navigation>('home');
+    const setStyle = (state: boolean)=>{
+        if (state) {
+            return 'footer-navigator-now';
+        } else {
+            return 'footer-navigator';
+        }
+    }
     return (
         <div className="footer">
-            <Link to="/" className="footer-navigator"><GrHomeRounded/></Link>
-            <Link to="/search" className="footer-navigator"><AiOutlineSearch/></Link>
-            <Link to="/bookmark" className="footer-navigator"><FiBookmark/></Link>
-            <Link to="mypage" className="footer-navigator"><HiUserCircle/></Link>
+            <Link to="/" className="footer-navigator"><GrHomeRounded className="icons"/></Link>
+            <Link to="/search" className="footer-navigator"><AiOutlineSearch className="icons"/></Link>
+            <Link to="/bookmark" className="footer-navigator"><FiBookmark className="icons"/></Link>
+            <Link to="mypage" className="footer-navigator"><HiUserCircle className="icons"/></Link>
         </div>
     )
 }
