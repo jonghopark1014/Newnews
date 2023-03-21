8b7222d71c3bddd276a027d0db63a3bbb61bff94
이성복 <seungbok3240@gmail.com>
2023-03-21 13:15:19 +0900

    [FEAT] 회원가입

--- specialization/src/main/java/com/ssafy/specialization/SpecializationApplication.java
+++ specialization/src/main/java/com/ssafy/specialization/SpecializationApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.specialization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpecializationApplication {
@@ -10,4 +13,8 @@ public class SpecializationApplication {
 		SpringApplication.run(SpecializationApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
---
+++ specialization/src/main/java/com/ssafy/specialization/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.ssafy.specialization.controller;
+
+import com.ssafy.specialization.dto.JoinRequestDto;
+import com.ssafy.specialization.response.Response;
+import com.ssafy.specialization.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity join(@RequestBody JoinRequestDto requestDto) {
+        userService.join(requestDto);
+        return Response.success(HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity badCredentialsException(BadCredentialsException e) {
+        return Response.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity illegalStateException(IllegalStateException e) {
+        return Response.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+}
