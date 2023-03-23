2577acfe4eaff7aedf269e72ab2b587475ad93a0
ssg-js <likemath1997@gmail.com>
2023-03-23 11:11:16 +0900

    FEAT: FE_메인페이지_토픽추가기능
    선택되었던 토픽이 없어질 시 기본 토픽으로 변경되도록 수정

--- newnews/src/components/mainpage/MainPageTab.tsx
+++ newnews/src/components/mainpage/MainPageTab.tsx
@@ -2,20 +2,26 @@ import { MainPageTabTopic } from "./MainPageTabTopic";
 import "@/styles/MainPage.scss"
 import { useRecoilState } from "recoil"
 import { topicAtom, topicStateType } from "../../stores/NewsTopics"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsPlus } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export function MainPageTab(){
     const [topicState, setTopicState] = useRecoilState<topicStateType>(topicAtom);
-    const [topicLen, setTopicLen] = useState<number>(topicState.focused.length)
-
+    const [topicLen, setTopicLen] = useState<number>(topicState.focused.length);
+    
     const setState = (f: string)=>{
         const state = {...topicState};
         state.focused = f;
         setTopicState(state);
     }
-
+    
+    useEffect(()=>{
+        if (topicState.topics.indexOf(topicState.focused) == -1) {
+            setState("연관뉴스")
+        }
+    }, [topicState])
+    
     return (
         <div className="main-page-tab">
             <div className="main-page-tab-topic">
