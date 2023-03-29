c7355b990096bad60f4bb471f9ca890195f3348b
왕현석 <sjj02055@gmail.com>
2023-03-29 13:28:26 +0900

    FEAT : Category, Press Getter설정

--- specialization/src/main/java/com/ssafy/specialization/entity/enums/Category.java
+++ specialization/src/main/java/com/ssafy/specialization/entity/enums/Category.java
@@ -1,7 +1,18 @@
 package com.ssafy.specialization.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
     //TODO
     /* 추후 카테고리 생성 예정 */
-    POL, ECO, SOC, LIF, IT
+    POL("정치"), ECO("경제"),
+    SOC("사회"), LIF("생활/문화"),
+    IT("IT/과학");
+
+    private String krName;
+
+    Category(String krName) {
+        this.krName=krName;
+    }
 }
