e5514b66df8bff62741e580fede0f30fff237fcd
이기용 <kanfs2022@gmail.com>
2023-04-05 13:21:41 +0900

    FEAT : 회원탈퇴 API 생성

--- newnews/src/hooks/mypage/useUserDropout.ts
+++ newnews/src/hooks/mypage/useUserDropout.ts
@@ -4,7 +4,7 @@ import { useMutation } from "react-query";
 
 const API_URL = '/api/user';
 
-const fetcher = (variables: { userId: number, password: string}) => axios.post(SERVER_URL + API_URL, { userId: variables.userId, password: variables.password }).then(({ data }) => data)
+const fetcher = (variables: { userId: number| null | undefined, password: string | null | undefined}) => axios.patch(SERVER_URL + API_URL, { userId: variables.userId, password: variables.password }).then(({ data }) => data)
 
 
 // 공부 : https://devkkiri.com/post/b3fe8ba3-46df-4cf0-b260-2c862628c0d9
@@ -12,13 +12,13 @@ const fetcher = (variables: { userId: number, password: string}) => axios.post(S
  * 후속 뉴스 표지
  * @returns 
  */
-const useUserDropout = () => {
+const useUserDropout = ( userId: number| null | undefined, password: string | null | undefined) => {
     return useMutation(fetcher, {
         onSuccess: (data) => {
-            console.log('탈퇴성공', data);
+            console.log('탈퇴성공', data, userId, password);
         },
         onError: (error) => {
-            console.log('탈퇴에러', error);
+            console.log('탈퇴에러', error, userId, password);
         }
     });
 }
--- newnews/src/pages/mypage/MyPage.tsx
+++ newnews/src/pages/mypage/MyPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, PureComponent } from "react";
+import { useEffect, useState, PureComponent, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
@@ -8,6 +8,9 @@ import { LoginState } from '@/states/LoginState';
 
 import styles from '@/styles/mypage/MyPage.module.scss';
 import { Button } from "@/components/Button";
+import MemberShipModal from "@/components/membership/MemberShipModal";
+import Modal from "@/components/bell/Modal";
+import useUserDropout from "@/hooks/mypage/useUserDropout";
 
 interface info {
     id: string,
@@ -17,15 +20,21 @@ interface info {
 
 
 
-const SEC = 3
+const SEC = 2
 
 export function MyPage(){
     const navigate = useNavigate()
-    const [alarm, setAlarm] = useState<null | string>(null);
+    const [alarm, setAlarm] = useState<boolean>(false);
+    const [modal, setModal] = useState<boolean>(false)
     const [myInfo, setMyInfo] = useState<info | null>(null)
     const isLogin = useRecoilValue(LoginState)
     const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
+    const userId = isLogin[0].id
+    const password = isLogin[0].password
+
     const defaultTopics = ["경제", "정치", "사회", "생활/문화", "IT/과학"];
+
+    const userDropout = useUserDropout(userId, password)
 
     const data = [
         {
@@ -56,13 +65,26 @@ export function MyPage(){
     ];
     useEffect(() => {
         if (!isLogin[0]?.isLogin) {
-            setAlarm("로그인이 필요한 페이지 입니다. 로그인 페이지로 이동합니다.")
+            setAlarm(!alarm)
             setTimeout(()=>{
-                setAlarm(null);
+                setAlarm(!alarm);
                 navigate('/login');
             }, SEC * 1000)
         }
     }, [isLogin, navigate])
+
+    const modalHandle = () => {
+        return setModal(!modal)
+    }
+    const onClcikWithdrawal = () =>{
+        userDropout.mutate({ userId: userId, password: password})
+        // navigate('/')
+    }
+
+
+    const onLgoinToggleModal = useCallback (() => {
+            setAlarm(!alarm);
+    }, [alarm])
 
     const onClickLogout = () =>{
         setIsLoggedIn([{isLogin: false, username: null, password: null, id: null}])
@@ -78,13 +100,11 @@ export function MyPage(){
                 </RadarChart>
             </ResponsiveContainer>
             <div>
-                <Button children="탈퇴하기" width={140} onClick={()=>{}} onKeyDown={()=>{}}></Button>
-                <BiLogOut onClick={() => {onClickLogout()}} />
+                <Button children="탈퇴하기" width={100} onClick={()=>{modalHandle()}} onKeyDown={()=>{}}></Button>
+                <BiLogOut onClick={() => {onClickLogout()}} className={styles.icons}/>
             </div>
-            {alarm && 
-                <div className={styles.alarm}>
-                    <h3>{alarm}</h3>
-                </div>}
+            {modal && <Modal children={"정말로 탈퇴하시겠습니까?"} onClickToggleModal={modalHandle} onClickChoice={onClcikWithdrawal}/>}
+            {alarm && <MemberShipModal children={"로그인이 필요한 페이지 입니다. 로그인 페이지로 이동합니다."} onClickToggleModal={onLgoinToggleModal}/>}
         </section>
     )
 }
