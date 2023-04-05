915faf1171b35556c46481b3e128c84242806d8e
이성복 <seungbok3240@gmail.com>
2023-04-05 22:40:23 +0900

    FIX: delete bookmark

--- specialization/src/main/java/com/ssafy/specialization/repository/BookmarkRepository.java
+++ specialization/src/main/java/com/ssafy/specialization/repository/BookmarkRepository.java
@@ -2,11 +2,17 @@ package com.ssafy.specialization.repository;
 
 import com.ssafy.specialization.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    int deleteByUserIdAndNewsId(Long userId, Long newsId);
+
+    @Modifying
+    @Query("delete from Bookmark b where b.news.id = :newsId and b.user.id = :userId")
+    int deleteByUserIdAndNewsId(@Param("userId") Long userId, @Param("newsId") Long newsId);
     Optional<Bookmark> findByUserIdAndNewsId(Long userId, Long newsId);
     Optional<Bookmark> findByUserUsernameAndNewsId(String username, Long newsId);
 }
