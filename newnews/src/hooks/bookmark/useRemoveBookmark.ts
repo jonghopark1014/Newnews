65f7dee927c2fc09d58ce016d4a5e022260193aa
ssg-js <likemath1997@gmail.com>
2023-04-05 22:57:03 +0900

    FIX: FE_북마크 제거 요청 코드 수정

--- newnews/src/hooks/bookmark/useRemoveBookmark.ts
+++ newnews/src/hooks/bookmark/useRemoveBookmark.ts
@@ -5,7 +5,7 @@ import { MAIN_NEWS_DETAIL } from "../main/useMainNewsDetail";
 
 const API_URL = '/api/news/bookmark';
 
-const fetcher = (variables: { userId: number| null | undefined, newsId: number }) => axios.delete(SERVER_URL + API_URL+ `/${variables.newsId}/${variables.userId}`).then(({ data }) => data)
+const fetcher = (variables: { userId: number| null | undefined, newsId: number }) => axios.delete(SERVER_URL + API_URL+ `/${variables.userId}/${variables.newsId}`).then(({ data }) => data)
 
 // 공부 : https://devkkiri.com/post/b3fe8ba3-46df-4cf0-b260-2c862628c0d9
 const useRemoveBookmark = (userId: number| null | undefined, newsId: number) => {
