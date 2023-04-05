1030e2aee29fd0177538da11798464e23bf111ef
왕현석 <sjj02055@gmail.com>
2023-04-05 14:56:42 +0900

    FEAT : 유저 뉴스 경향 API 구현

---
+++ specialization/src/main/java/com/ssafy/specialization/repository/WatchedRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.ssafy.specialization.repository;
+
+import com.ssafy.specialization.dto.WatchedResponseDto;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.EntityManager;
+import java.math.BigInteger;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class WatchedRepositoryImpl implements CustomWatchedRepository{
+    private final EntityManager em;
+
+    @Override
+    public WatchedResponseDto countWatchedCategory(Long userId) {
+        String sql = "select n.dtype, count(n.dtype) from watched w left join news n on n.news_id = w.news_id where w.user_id=? group by n.dtype";
+
+        List<Object[]> resultList = em.createNativeQuery(sql).setParameter(1,userId).getResultList();
+        WatchedResponseDto watchedResponseDto = new WatchedResponseDto();
+
+        for (Object[] objects : resultList) {
+            String type = (String) objects[0];
+            BigInteger temp = (BigInteger) objects[1];
+            Long count = temp.longValue();
+
+            switch (type) {
+                case "Economy":
+                    watchedResponseDto.setEconomyNews(count);
+                    break;
+                case "Politics":
+                    watchedResponseDto.setPoliticsNews(count);
+                    break;
+                case "Society":
+                    watchedResponseDto.setSocietyNews(count);
+                    break;
+                case "LifeAndCulture":
+                    watchedResponseDto.setLifeAndCultureNews(count);
+                    break;
+                case "ItAndScience":
+                    watchedResponseDto.setItAndScienceNews(count);
+                    break;
+            }
+        }
+
+        return watchedResponseDto;
+    }
+}
