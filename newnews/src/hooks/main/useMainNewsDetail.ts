2133ae43ef36c1c6cdf9d5e6217f117183a92d8f
ssg-js <likemath1997@gmail.com>
2023-04-05 10:21:14 +0900

    FIX: FE_뉴스기사 요청 시 파라미터 변수 수정

--- newnews/src/hooks/main/useMainNewsDetail.ts
+++ newnews/src/hooks/main/useMainNewsDetail.ts
@@ -1,12 +1,14 @@
+import { LoginState } from "@/states/LoginState";
 import { SERVER_URL } from "@/utils/urls";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useRecoilValue } from "recoil";
 
 export const MAIN_NEWS_DETAIL = '/main/detail';
 
 const API_URL = '/api/news/details';
 
-const fetcher = (newsId: number, categoryId: number) => axios.get(SERVER_URL + API_URL + `/${newsId}/${categoryId}`).then(({ data }) => data);
+const fetcher = (newsId: number, body: string) => axios.get(SERVER_URL + API_URL + `/${newsId}` + body).then(({ data }) => data);
 
 /**
  * userId: useQuery안에 fetcher(axios요청)에 넣을 파라미터 값(들)
@@ -15,8 +17,17 @@ const fetcher = (newsId: number, categoryId: number) => axios.get(SERVER_URL + A
  * @returns [data, dataUpdatedAt, error, errorUpdatedAt, failureCount, failureReason, isError, isFetched, isFetchedAfterMount, isFetching, isPaused, isLoading, isLoadingError, isPlaceholderData, isPreviousData, isRefetchError, isRefetching, isInitialLoading, isStale, isSuccess, refetch, remove, status, fetchStatus]
  */
 const useMainNewsDetail = (newsId: number, categoryId: number) => {
+    const isLogin = useRecoilValue(LoginState)[0]
+    let body: string
+    if (isLogin.id) {
+        body = `/${categoryId}/${isLogin.id}`
+    } else {
+        body = `/${categoryId}`
+    }
+
+
     // useQuery의 첫 번째 파라미터는 query key, query는 쿼리문마다 달라야함(주의!!), 쿼리를 보내는 트리거라고 생각하면 됨
-    return useQuery([MAIN_NEWS_DETAIL], () => fetcher(newsId, categoryId), {
+    return useQuery([MAIN_NEWS_DETAIL], () => fetcher(newsId, body), {
         // staleTime: 데이터 요청을 보내는 시간 간격(ms)
         // cacheTime: 데이터를 캐시에 저장할 시간(ms), 데이터 요청을 보내오는 동안 캐시에 저장된 데이터를 보여줌
         // 다른 옵션은 그대로 써도 될 것 같아유.
