e5f00b77dd1ff34eaf90ba2d4f7ec742c41675ed
이성복 <seungbok3240@gmail.com>
2023-04-07 00:32:55 +0900

    FIX: notification list response

--- specialization/src/main/java/com/ssafy/specialization/dto/NotificationListResponseDto.java
+++ specialization/src/main/java/com/ssafy/specialization/dto/NotificationListResponseDto.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 @Getter
 public class NotificationListResponseDto {
 
+    private String category;
     private Long newsId;
     private Long preNewsId;
     private String preNewsTitle;
 
-    public NotificationListResponseDto(Long newsId, Long preNewsId, String preNewsTitle) {
+    public NotificationListResponseDto(String category, Long newsId, Long preNewsId, String preNewsTitle) {
+        this.category = category;
         this.newsId = newsId;
         this.preNewsId = preNewsId;
         this.preNewsTitle = preNewsTitle;
