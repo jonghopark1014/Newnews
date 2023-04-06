154ad30eb711cc92e4035b00d199f68910c320b3
이기용 <kanfs2022@gmail.com>
2023-04-06 22:03:36 +0900

    FIX : CSS_수정

--- newnews/src/pages/bell/BellPages.tsx
+++ newnews/src/pages/bell/BellPages.tsx
@@ -5,7 +5,6 @@ import { LoginState } from "@/states/LoginState";
 import { BellHeader } from "@/components/bell/BellHeader";
 import { BellComponents } from "@/components/bell/BellComponents";
 import useBellList from "@/hooks/bell/useBellList";
-// import useBellDelete from "@/hooks/bell/useBellDelete";
 
 import styles from "@/styles/bell/Bellpages.module.scss"
 
@@ -23,13 +22,12 @@ interface Iprops{
  * @returns 알림페이지
  */
 export function BellPages(){
-    const bellList = useBellList()
-    // const bellDelete= useBellDelete()
     
     const isLogin = useRecoilValue(LoginState)
     const userId = isLogin[0].id
-
     const [data, setData] = useState<Iprops[]>([])
+    
+    const bellList = useBellList()
     /**
      * 페이지 랜더링하자마자 알람을 가져오기
     */
@@ -40,14 +38,6 @@ export function BellPages(){
             }
         })
     })
-    // const newsId = data[0].newsId
-    // useEffect(() =>{
-    //     bellDelete.mutate({ userId: userId, newsId: newsId },{
-    //         onSuccess:(data) =>{
-                
-    //         }
-    //     })
-    // },[])
 
     return (
         <section className={styles.testObj}>
