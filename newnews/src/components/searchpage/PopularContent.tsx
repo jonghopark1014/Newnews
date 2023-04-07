6890ad9811e188cd6187cea079acd17b216cdd03
이기용 <kanfs2022@gmail.com>
2023-04-07 09:41:00 +0900

    FIX : 버그수정

--- newnews/src/components/searchpage/PopularContent.tsx
+++ newnews/src/components/searchpage/PopularContent.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useSearchKeywordRank from "@/hooks/search/useSearchKeywordRank";
 
 import styles from "@/styles/search/SearchPages.module.scss"
+import { useNavigate } from "react-router-dom";
 
 interface Iprops{
     popularData: Array<HTMLElement>,
@@ -20,18 +21,23 @@ export function PopularContent( ) {
     const [popularData, setPopularData] = useState<PopularProps[] | undefined>()
 
     const searchKeywordRank = useSearchKeywordRank()
-
+    const navigate = useNavigate()
     useEffect(() => {
         if (searchKeywordRank.isSuccess) {
             setPopularData(searchKeywordRank.data.data)
             }
     }, [searchKeywordRank]);
     console.log(popularData)
+
+    const onClickSearch = (keyword : string) => {
+        navigate('/result', { state: { keyword: keyword }})
+    }
+
     return (
         <div>
             <div className={styles.popularContentGrid} >
                 { popularData !== undefined && popularData.map((item, index) =>
-                    <p className={styles.popularContent} key={index}> {item.rank}. {item.keyword} </p>
+                    <p className={styles.popularContent} key={index} onClick={() => {onClickSearch(item.keyword)}}> {item.rank}. {item.keyword} </p>
                 )}
                 { popularData === undefined && <p className={styles.popularContent}>추천검색어가 없습니다.</p>}
             </div>
